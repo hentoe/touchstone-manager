d05cc76a19973ba595a2855921af86c7d9db0fa4
Hendrik Tödter <hendrik.toedter@tu-dresden.de>
2024-04-24 12:51:26 +0200

    Fix tests for MeasurementListView and MeasurementDetailView. Add templates for both views.

--- touchstone_manager/aero/tests/test_views.py
+++ touchstone_manager/aero/tests/test_views.py
@@ -9,10 +9,13 @@ from django.urls import reverse
 
 from touchstone_manager.aero.tests.factories import MaterialFactory
 from touchstone_manager.aero.tests.factories import MaterialSampleFactory
+from touchstone_manager.aero.tests.factories import MeasurementFactory
 from touchstone_manager.aero.views import material_detail_view
 from touchstone_manager.aero.views import material_list_view
 from touchstone_manager.aero.views import material_sample_detail_view
 from touchstone_manager.aero.views import material_sample_list_view
+from touchstone_manager.aero.views import measurement_detail_view
+from touchstone_manager.aero.views import measurement_list_view
 from touchstone_manager.users.models import User
 from touchstone_manager.users.tests.factories import UserFactory
 
@@ -113,21 +116,44 @@ class TestMaterialSampleDetailView:
 
 class TestMeasurementListView:
     def test_authenticated(self, user: User, rf: RequestFactory):
-        total_material_num = 10
-        MaterialSampleFactory.create_batch(total_material_num)
+        total_object_num = 10
+        MeasurementFactory.create_batch(total_object_num)
         url = reverse("aero:measurements")
         request = rf.get(url)
         request.user = user
-        response = material_sample_list_view(request)
+        response = measurement_list_view(request)
 
         assert response.status_code == HTTPStatus.OK
-        assert len(response.context_data["object_list"]) == total_material_num
+        assert len(response.context_data["object_list"]) == total_object_num
 
     def test_not_authenticated(self, user: User, rf: RequestFactory):
         url = reverse("aero:measurements")
         request = rf.get(url)
         request.user = AnonymousUser()
-        response = material_sample_list_view(request)
+        response = measurement_list_view(request)
+        login_url = reverse(settings.LOGIN_URL)
+
+        assert isinstance(response, HttpResponseRedirect)
+        assert response.status_code == HTTPStatus.FOUND
+        assert response.url == f"{login_url}?next={url}"
+
+
+class TestMeasurementDetailView:
+    def test_authenticated(self, user: User, rf: RequestFactory):
+        measurement = MeasurementFactory.create()
+        url = reverse("aero:measurement-detail", kwargs={"pk": measurement.pk})
+        request = rf.get(url)
+        request.user = user
+        response = measurement_detail_view(request, pk=measurement.pk)
+
+        assert response.status_code == HTTPStatus.OK
+
+    def test_not_authenticated(self, user: User, rf: RequestFactory):
+        measurement = MeasurementFactory.create()
+        url = reverse("aero:measurement-detail", kwargs={"pk": measurement.pk})
+        request = rf.get(url)
+        request.user = AnonymousUser()
+        response = measurement_detail_view(request, pk=measurement.pk)
         login_url = reverse(settings.LOGIN_URL)
 
         assert isinstance(response, HttpResponseRedirect)
