189baa657232ef131f778aead38c48deb5dd5e92
Hendrik Tödter <hendrik.toedter@tu-dresden.de>
2024-04-25 13:50:39 +0200

    Add error handling

--- touchstone_manager/aero/tasks.py
+++ touchstone_manager/aero/tasks.py
@@ -1,4 +1,10 @@
+import logging
+
 from celery import shared_task
+from django.core.exceptions import ObjectDoesNotExist
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 @shared_task
@@ -7,8 +13,23 @@ def calculate_mean_s21(measurement_id):
 
     from .models import Measurement
 
-    measurement = Measurement.objects.get(id=measurement_id)
-    network = Network(measurement.measurement_file)
-    s21 = network.s21.s_db
-    measurement.mean_s21 = s21.mean()
-    measurement.save()
+    try:
+        measurement = Measurement.objects.get(id=measurement_id)
+    except ObjectDoesNotExist:
+        logger.exception("Measurement with id %s does not exist.", measurement_id)
+        return
+
+    try:
+        network = Network(measurement.measurement_file.path)
+        s21 = network.s21.s_db
+        measurement.mean_s21 = s21.mean()
+        measurement.save()
+        logger.info(
+            "Successfully calculated mean S21 for measurement id %s.",
+            measurement_id,
+        )
+    except Exception:
+        logger.exception(
+            "Failed to process the measurement file for id %s.",
+            measurement_id,
+        )
