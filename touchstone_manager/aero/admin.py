e6e43b213817b8565b94a9c6f15ec7d299ecd5d8
Hendrik Tödter <hendrik.toedter@tu-dresden.de>
2024-04-26 12:23:29 +0200

    Fix triggering updating measurement_file via admin panel.

--- touchstone_manager/aero/admin.py
+++ touchstone_manager/aero/admin.py
@@ -4,6 +4,18 @@ from .models import Material
 from .models import MaterialSample
 from .models import Measurement
 
+
+@admin.register(Measurement)
+class MeasurementAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change) -> None:
+        if change:
+            if form.changed_data:
+                obj.save(update_fields=form.changed_data)
+            else:
+                obj.save()
+        else:
+            obj.save()
+
+
 admin.site.register(Material)
 admin.site.register(MaterialSample)
-admin.site.register(Measurement)
--- touchstone_manager/aero/signals.py
+++ touchstone_manager/aero/signals.py
@@ -8,5 +8,10 @@ from .tasks import process_measurement_file
 @receiver(post_save, sender=Measurement)
 def trigger_file_processing(sender, instance, created, **kwargs):
     # Trigger task only if the FileField is updated or on creation of a new instance
-    if created or "measurement_file" in kwargs.get("update_fields", []):
+    update_fields = kwargs.get("update_fields")
+
+    if update_fields and "measurement_file" in update_fields:
+        process_measurement_file.delay(instance.pk)
+
+    if created and instance.measurement_file.path:
         process_measurement_file.delay(instance.pk)
