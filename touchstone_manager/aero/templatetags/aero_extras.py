704414336fc632a8794161bf94544ac49c384e21
Hendrik Tödter <hendrik.toedter@tu-dresden.de>
2024-07-04 12:51:47 +0200

    Show Material name and measurement id in graph.

--- touchstone_manager/aero/templatetags/aero_extras.py
+++ touchstone_manager/aero/templatetags/aero_extras.py
@@ -2,10 +2,35 @@ import json
 
 from django import template
 from django.core.serializers.json import DjangoJSONEncoder
+from django.db.models.fields.files import FieldFile
 
 register = template.Library()
 
 
 @register.filter
 def to_json(value):
-    return json.dumps(list(value.values()), cls=DjangoJSONEncoder)
+    if hasattr(value, "all"):
+        # for QuerySet of objects
+        data = [obj_to_dict(obj) for obj in value]
+    else:
+        # for single object
+        data = obj_to_dict(value)
+    return json.dumps(data, cls=DjangoJSONEncoder)
+
+
+def obj_to_dict(obj):
+    data = {}
+    for field in obj._meta.get_fields():  # noqa: SLF001
+        if field.is_relation and field.many_to_one:
+            related_obj = getattr(obj, field.name, None)
+            if related_obj:
+                data[field.name] = obj_to_dict(related_obj)
+            else:
+                data[field.name] = None
+        else:
+            value = getattr(obj, field.name, None)
+            if isinstance(value, FieldFile):
+                data[field.name] = value.url if value else None
+            else:
+                data[field.name] = value
+    return data
